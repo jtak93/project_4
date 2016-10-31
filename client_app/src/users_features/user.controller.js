13223cea9e4b609a11163f3c1f5b8e3de9c73861
james tak <jtak93@gmail.com>
2016-10-31 11:14:41 -0700

    handle client side password match for signup

--- client_app/src/users_features/user.controller.js
+++ client_app/src/users_features/user.controller.js
@@ -5,25 +5,34 @@
     .module("app")
     .controller("UserController", UserController);
 
-  UserController.$inject = ["$log", "UserService"];
+  UserController.$inject = ["$log", "UserService", "$state"];
 
-  function UserController($log, UserService) {
+  function UserController($log, UserService, $state) {
     var vm = this;
     vm.login = login;
     vm.signUp = signUp;
+    vm.noMatch = null;
 
     function login() {
-      return UserService.login(vm.username, vm.password)
+      UserService.login(vm.username, vm.password);
+      $state.go('home')
     }
 
     function signUp() {
-      var newUser = {
-        username: vm.username,
-        email: vm.email,
-        password: vm.password,
-        pwConfirm: vm.pwConfirm
+      if (vm.password === vm.pwConfirm) {
+        var newUser = {
+          username: vm.username,
+          email: vm.email,
+          password: vm.password,
+          pwConfirm: vm.pwConfirm
+        }
+        return UserService.signUp(newUser)
       }
-      return UserService.signUp(newUser)
+      // TODO show user passwords dont match
+      else {
+        vm.noMatch = true;
+        return console.log("passwords dont match")
+      }
     }
   }
 
