5b04925be9b0d0527664730e34d26f4bb343c3a2
james tak <jtak93@gmail.com>
2016-10-31 16:30:56 -0700

    client-side bet slip

--- client_app/public/js/all.js
+++ client_app/public/js/all.js
@@ -235,13 +235,23 @@
   function HomeController($log, MatchService) {
     var vm = this;
     vm.all = all();
-
-    vm.bets = [1, 2, 3, 4]
-    console.log(vm.all);
+    vm.bets = [];
+    vm.betTeam1 = betTeam1;
+    vm.betTeam2 = betTeam2;
 
     function all() {
       MatchService.all()
         .then( matches => vm.matches = matches.data)
+    }
+
+    function betTeam1(match) {
+      $log.info(match)
+      vm.bets.push(match)
+    }
+
+    function betTeam2(match) {
+      $log.info(match)
+      vm.bets.push(match)
     }
   }
 
