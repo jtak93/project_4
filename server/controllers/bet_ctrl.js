6c90a848b64af7c088fc61acc570abeeea687d28
james tak <jtak93@gmail.com>
2016-11-02 13:07:00 -0700

    match persists bets

--- server/controllers/bet_ctrl.js
+++ server/controllers/bet_ctrl.js
@@ -20,12 +20,22 @@ function create(req, res, next) {
         user.balance -= risks[idx];
         user.inPlay += risks[idx];
         user.save((err, user) => {
-          console.log(user);
           Match.findOne( {_id: bet._id} )
             .then(match => {
               match.bets.push(newBet);
+              // get team pick
+              console.log(bet.teams)
+              console.log(bet.teamPick)
+              var teamNum = bet.teams.indexOf(bet.teamPick)
+              console.log(teamNum)
+              if  (teamNum === 0) {
+                match.t1bet += risks[idx]
+              } else if (teamNum === 1) {
+                match.t2bet += risks[idx]
+              }
               match.save(err => {
                 if (err) return err
+                console.log(match)
                 next();
               })
             })
