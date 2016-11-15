4e57f44aed23062ddc6e2a20f6a65d99c0199214
james tak <jtak93@gmail.com>
2016-11-15 15:11:27 -0800

    prevent overbetting

--- server/controllers/bet_ctrl.js
+++ server/controllers/bet_ctrl.js
@@ -1,6 +1,7 @@
 var User = require('../models/user');
 var Match = require('../models/match');
 var Bet = require('../models/bet');
+var jwt = require('jsonwebtoken');
 var itemsProcessed;
 var betSlip;
 var risks;
@@ -14,48 +15,59 @@ function create(req, res, next) {
   betSlip = req.body.betSlip;
   risks = req.body.risks;
   user = req.body.user;
-  riskTot = riskSum()
-  betSlip.forEach( (bet, idx) => {
-    var newBet = {
-      matchId: bet._id,
-      userId: user._id,
-      risk: risks[idx],
-      team: bet.teamPick
-    }
-    Match.findOne( {_id: bet._id} )
-    .then( match => {
-      match.bets.push(newBet)
-      var teamNum = bet.teams.indexOf(bet.teamPick)
-      if  (teamNum === 0) {
-        match.t1bet += risks[idx]
-      } else if (teamNum === 1) {
-        match.t2bet += risks[idx]
+  riskTot = riskSum();
+  console.log("user:", user)
+  // return user token and error if bet > balance
+  console.log(riskTot, user.balance)
+  if (riskTot > user.balance) {
+    User.findOne({_id: req.body.user._id})
+      .then((user) => {
+        var token = jwt.sign({ user: user }, secret, jwtOptions);
+        return res.json({
+          token: token,
+          err: "Bet Exceeds Balance!"
+        });
+      });
+  } else {
+    betSlip.forEach( (bet, idx) => {
+      var newBet = {
+        matchId: bet._id,
+        userId: user._id,
+        risk: risks[idx],
+        team: bet.teamPick
       }
-      return match.save( err => {
-        if (err) return err.message
-      })
-    })
-    .then( () => {
       Match.findOne( {_id: bet._id} )
       .then( match => {
-        User.findOne( {_id: user._id} )
-        .then( oldUser => {
-          console.log("user: ", oldUser)
-          console.log(riskSum())
-          oldUser.balance -= riskTot;
-          oldUser.inPlay += riskTot;
-          console.log("user: ", oldUser)
-          oldUser.save( err => {
-            console.log("User saved!");
-            itemsProcessed++;
-            if(itemsProcessed === betSlip.length) {
-              next();
-            }
+        match.bets.push(newBet)
+        var teamNum = bet.teams.indexOf(bet.teamPick)
+        if  (teamNum === 0) {
+          match.t1bet += risks[idx]
+        } else if (teamNum === 1) {
+          match.t2bet += risks[idx]
+        }
+        return match.save( err => {
+          if (err) return err.message
+        })
+      })
+      .then( () => {
+        Match.findOne( {_id: bet._id} )
+        .then( match => {
+          User.findOne( {_id: user._id} )
+          .then( oldUser => {
+            oldUser.balance -= riskTot;
+            oldUser.inPlay += riskTot;
+            oldUser.save( err => {
+              console.log("User saved!");
+              itemsProcessed++;
+              if(itemsProcessed === betSlip.length) {
+                next();
+              }
+            })
           })
         })
       })
     })
-  })
+  }
 }
 // TODO USE PROMISE.ALL TO HANDLE THE MULTIPLE BETS
 
--- server/public/js/services/bet.service.js
+++ server/public/js/services/bet.service.js
@@ -5,9 +5,9 @@
     .module("app")
     .factory("BetService", BetService);
 
-  BetService.$inject = ["$http", "MatchService", "$window", "$log", "UserService"];
+  BetService.$inject = ["$http", "MatchService", "$window", "$log", "UserService", "AuthTokenService"];
 
-  function BetService($http, MatchService, $window, $log, UserService) {
+  function BetService($http, MatchService, $window, $log, UserService, AuthTokenService) {
 
     // var baseUrl = process.env.BASE_URL;
     var betSlip = [];
@@ -51,15 +51,32 @@
     }
 
     function placeBet(betSlip, risks, user) {
-      var url = `${baseUrl}/bets/create`
-      return $http.post(url, {betSlip, risks, user})
-                  .then((res) => {
-                    return res.data.token;
-                  });
+      if (validateBet(risks, user)) {
+        var url = `${baseUrl}/bets/create`
+        return $http.post(url, {betSlip, risks, user})
+                    .then((res) => {
+                      return res.data.token;
+                    });
+      } else {
+        return AuthTokenService.getToken()
+      }
     }
 
     function decode(token) {
       return JSON.parse($window.atob(token.split('.')[1]));
+    }
+
+    function validateBet(risks, user) {
+      console.log(riskSum(risks), user.balance)
+      return riskSum(risks) < user.balance ? true : false
+    }
+
+    function riskSum(risks) {
+      var sum = 0
+      risks.forEach(risk => {
+        sum += risk
+      })
+      return sum
     }
 
     return betService;
